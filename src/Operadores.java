27db11d4865b23933c96d8726e5bb1e14041a9f1
RafaelLMartins <rafaelluism@hotmail.com>
2023-03-13 17:33:35 -0300

    Operadores 3. Operadores unários.

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,6 +1,6 @@
 public class Operadores {
     public static void main(String[] args) {
-        //classe Operadores.java
+     /*   //classe Operadores.java
         String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println("Nome completo: " + nomeCompleto);
 
@@ -19,7 +19,34 @@ public class Operadores {
         System.out.println(concatenacao);
 
         concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+        System.out.println(concatenacao);*/
 
+        //classe Operadores.java
+        int numero = 5;
+        System.out.println("Usando Operador unário de valor negativo (-): " + numero);
+
+        numero = - numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero);
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+
+//incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+//incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+//ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
     }
 }
