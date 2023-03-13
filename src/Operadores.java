c0337e687bf7522195aac4447a7a5764e6bf2ec9
RafaelLMartins <rafaelluism@hotmail.com>
2023-03-13 20:30:05 -0300

    Operadores 6. Operadores ternário relacionais.

--- src/Operadores.java
+++ src/Operadores.java
@@ -75,5 +75,25 @@ public class Operadores {
         String resultado = (a == b) ? "verdadeiro" : "false";
 
         System.out.println(resultado);
+
+        String nomeUm= "Rafael";
+        String nomeDois="Rafael";
+        System.out.println(nomeUm==nomeDois);
+        System.out.println(nomeUm.equals(nomeDois));
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("Numero 1 é igual ao numero 2? "+simNao);
+
+        if(numero1 < numero2){
+            System.out.println("Essa condição é verdadeira ");
+        }
+
+        simNao= numero1 != numero2;
+        System.out.println("Numero 1 é diferente de numero 2? "+simNao);
+
+        simNao= numero1 > numero2;
+        System.out.println("Numero 1 é maior que numero 2? "+simNao);
     }
 }
